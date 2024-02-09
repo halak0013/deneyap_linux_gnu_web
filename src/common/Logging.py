60724cd045441bf44a89412b131ffab3abc38e75
halak0013 <zumruduanka0013@gmail.com>
2024-02-09 12:55:48 +0300

    logging format changing

--- src/common/Logging.py
+++ src/common/Logging.py
@@ -1,33 +1,23 @@
 import coloredlogs
 import logging
 from static.file_paths import Paths as p
-""" logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
-# Coloredlogs konfigürasyonu
-coloredlogs.install(level='DEBUG', fmt='%(asctime)s - %(levelname)s - %(message)s')
-
-# Logger oluşturma
-logger = logging.getLogger(__name__)
-
-def example_function():
-    logger.debug("Bu bir debug mesajıdır.")
-    logger.info("Bu bir bilgi mesajıdır.")
-    logger.warning("Bu bir uyarı mesajıdır.")
-    logger.error("Bu bir hata mesajıdır.")
-    logger.critical("Bu bir kritik hata mesajıdır.") """
+import datetime
 
 
 class Log:
     def __init__(self):
         p.file_check(p.log_path)
-        logging.basicConfig(filename=p.log_path + 'deneyap.log',
-                            encoding='utf-8', level=logging.DEBUG)
+        current_time = datetime.datetime.now().strftime("%y-%m-%d_%H:%M")
+        fmt='%(asctime)s - %(levelname)s - %(message)s'
+        logging.basicConfig(filename=p.log_path + f'{current_time}_deneyap.log',
+                            encoding='utf-8', level=logging.DEBUG,format=fmt)
         # Coloredlogs konfigürasyonu
         coloredlogs.install(
-            level='DEBUG', fmt='%(asctime)s - %(levelname)s - %(message)s')
+            level='DEBUG', fmt=fmt)
         # Logger oluşturma
         self.logger = logging.getLogger(__name__)
 
-    def log(self, msg, level):
+    def log(self, msg, level="i"):
         if level == "d":  # debug
             self.logger.debug(msg)
         elif level == "i":  # info
